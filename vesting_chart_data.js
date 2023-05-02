1edca687759cfd64c99afa030fe459e853476936
web-altcoinist <132388401+web-altcoinist@users.noreply.github.com>
2023-05-03 06:08:55 +0800

    Update vesting_chart_data.js

--- vesting_chart_data.js
+++ vesting_chart_data.js
@@ -1,4 +1,28 @@
-const vesting_data = [
+const data = {
+  labels: [
+    "Launch",
+    "M+3",
+    "M+4",
+    "M+5",
+    "M+6",
+    "M+7",
+    "M+8",
+    "M+9",
+    "M+10",
+    "M+11",
+    "M+12",
+    "M+13",
+    "M+14",
+    "M+15",
+    "M+16",
+    "M+17",
+    "M+18",
+    "M+19",
+    "M+20",
+    "M+21",
+    "M+22"
+],
+  datasets: [
     {
     label: "Private Sale",
     backgroundColor: "#003f5c",
@@ -496,4 +520,5 @@ const vesting_data = [
         1200000000
     ]
 },
-];
+]
+}
